13f71776b21703bcdafdc93670661c1e8db1d724
ccenlei <iacc7441@yahoo.co.jp>
2023-10-26 14:55:43 +0900

    add ThreadLocal Demo

---
+++ src/main/java/com/spring/bot/demo/utils/BookContext.java
@@ -0,0 +1,36 @@
+package com.spring.bot.demo.utils;
+
+import com.spring.bot.demo.entity.Book;
+
+import lombok.var;
+
+/**
+ * ThreadLocal Demo
+ * 
+ */
+public class BookContext implements AutoCloseable {
+
+    static final ThreadLocal<Book> ctx = new ThreadLocal<>();
+
+    public BookContext(Book book) {
+        ctx.set(book);
+    }
+
+    public static Book currBook() {
+        return ctx.get();
+    }
+
+    @Override
+    public void close() throws Exception {
+        ctx.remove();
+    }
+
+    public static void main(String[] args) {
+        try (var bctx = new BookContext(Book.builder().id(3).build())) {
+            Book book = BookContext.currBook();
+            System.out.println(book.getId());
+        } catch (Exception e) {
+            // TODO: handle exception
+        }
+    }
+}
